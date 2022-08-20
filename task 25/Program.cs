2aebb466013256c4e2a4b6c524fdfc8d5bef6939
Maxim Vashkin <vashkin.m@ya.ru>
2022-08-20 23:32:46 +0400

    add task #25

---
+++ task 25/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int NumPow (int number, int pow){
+int num = number;
+int i = 2;
+do {
+number=number*num;
+i++;
+}while(i<=pow);
+return number;
+}
+
+Console.WriteLine (NumPow(3,5));
+Console.WriteLine (NumPow(2,4));
