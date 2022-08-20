67cdc724545814df8c236aace1c632f91f359dfa
Maxim Vashkin <vashkin.m@ya.ru>
2022-08-21 00:40:04 +0400

    home 4

--- task 29/Program.cs
+++ task 29/Program.cs
@@ -1,11 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-int[] number = new int[8];
+﻿int[] number = new int[8];
 int i = 0;
 Console.Write("[");
 do
 {
     number[i] = new Random().Next(0, 100);
-
     Console.Write("" + number[i]);
     i++;
     if (i != 0 & i != number.Length)
@@ -13,5 +11,4 @@ do
         Console.Write(",");
     }
 } while (i <= number.Length - 1);
-
 Console.Write("]");
